36d3889e3eb2bff73df3e3319a0227f52c2c368f
agent19music <seanmotanya@gmail.com>
2024-01-10 18:54:09 +0300

    Done with assignment

--- server/app.py
+++ server/app.py
@@ -12,6 +12,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
+from flask import render_template
 
 @app.route('/')
 def home():
@@ -19,15 +20,24 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    if animal is None:
+        return make_response('<h1>No animal found with given id</h1>', 404)
+    return render_template('animal.html', animal=animal)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    if zookeeper is None:
+        return make_response('<h1>No zookeeper found with given id</h1>', 404)
+    return render_template('zookeeper.html', zookeeper=zookeeper)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get(id)
+    if enclosure is None:
+        return make_response('<h1>No enclosure found with given id</h1>', 404)
+    return render_template('enclosure.html', enclosure=enclosure)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -11,13 +11,24 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    birthday = db.Column(db.Date, nullable=False)
+    animals = db.relationship('Animal', backref='zookeeper', lazy=True)
+    
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(10), nullable=False)
+    open_to_visitors = db.Column(db.Boolean, nullable=False)
+    animals = db.relationship('Animal', backref='enclosure', lazy=True)
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    species = db.Column(db.String(170), nullable=False)
+    enclosure_id = db.Column(db.ForeignKey('enclosures.id' ), nullable=False)
+    zookeeper_id = db.Column(db.ForeignKey('zookeepers.id', ondelete='CASCADE'), nullable=False)
